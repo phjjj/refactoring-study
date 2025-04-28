f40de14e80261ee9b1f421bc4a230af9829f1929
박해준 <aaagowns@gmail.com>
2025-04-28 17:20:58 +0900

    코드 정리

--- 1주차/createStatementData.js
+++ 1주차/createStatementData.js
@@ -12,17 +12,6 @@ class PerformanceCalculator {
     }
 }
 
-function createPerformanceCalculator(aPerformance, aPlay) {
-    switch (aPlay.type) {
-        case 'tragedy':
-            return new TragedyCalculator(aPerformance, aPlay);
-        case 'comedy':
-            return new ComedyCalculator(aPerformance, aPlay);
-        default:
-            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
-    }
-}
-
 class TragedyCalculator extends PerformanceCalculator {
     get amount() {
         let result = 40000;
@@ -45,6 +34,17 @@ class ComedyCalculator extends PerformanceCalculator {
 
     get volumeCredits() {
         return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case 'tragedy':
+            return new TragedyCalculator(aPerformance, aPlay);
+        case 'comedy':
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
     }
 }
 
